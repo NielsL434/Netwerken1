8ffac34d905c2418b431561e83fc7eabbcc1b928
Niels Laarhoven <Niels.laarhoven@security.wortell.nl>
2023-06-18 17:22:06 +0200

    init

--- website/script.js
+++ website/script.js
@@ -1,13 +1,15 @@
 const mqttname = "0998070";
 const mqttpassword = "0998070";
 
+const cliendID = Math.random().toString(16).substr(2, 8);
+
 const options = {
     keepalive: 60,
-    clientId: 'chatuser' + Math.random().toString(16).substr(2, 8),
+    clientId: 'chatuser' + cliendID,
     reconnectPeriod: 1000,
-    connectTimeout: 30 * 1000,
-    username: mqttname,
-    password: mqttpassword
+    connectTimeout: 30 * 1000
+    //username: mqttname,
+    //password: mqttpassword
 };
 
 // Connect to the broker
@@ -21,14 +23,30 @@ client.on('connect', function () {
     client.subscribe(topic, function (err) {
         if (!err) {
             console.log('Subscribed to topic.');
+        } else {
+            console.log('Failed to subscribe to topic.' , err);
         }
     })
 })
 
+let lastSentMessage = null;
+
 client.on('message', function (topic, message) {
     // When a message is received, append it to the chat box
     const chatBox = document.getElementById('chatBox');
-    chatBox.innerHTML += `<p>${message.toString()}</p>`;
+    const messageElement = document.createElement('p');
+    messageElement.innerText = message.toString();
+
+    // Ignore if it is the last sent message
+    if (message.toString() === lastSentMessage) {
+        lastSentMessage = null;
+        return;
+    }
+    
+    // Set class as 'incoming' for incoming messages
+    messageElement.className = 'incoming';
+
+    chatBox.append(messageElement);
     chatBox.scrollTop = chatBox.scrollHeight; // Scroll to bottom
 })
 
@@ -36,8 +54,17 @@ const sendButton = document.getElementById('sendButton');
 const messageInput = document.getElementById('messageInput');
 
 sendButton.addEventListener('click', function() {
-    const message = messageInput.value;
+    const message = cliendID + ': ' + messageInput.value;
+    lastSentMessage = message;
     messageInput.value = ""; // Clear input field
+
+    // Append outgoing message to the chatbox
+    const chatBox = document.getElementById('chatBox');
+    const messageElement = document.createElement('p');
+    messageElement.innerText = message;
+    messageElement.className = 'outgoing';
+    chatBox.append(messageElement);
+    chatBox.scrollTop = chatBox.scrollHeight;
 
     // Publish message to topic
     client.publish(topic, message);
